e76c860ba30ec92f1d0c9a44eca46af5172633c4
J9-tops <j9.tops@gmail.com>
2025-02-21 18:21:19 +0100

    style: added responsive layouts

--- src/pages/ChatLayout.tsx
+++ src/pages/ChatLayout.tsx
@@ -3,7 +3,7 @@ import Input from "../components/Input";
 
 const ChatLayout = () => {
   return (
-    <main className="xs:h-[95%] mx-auto flex flex-col md:w-full lg:h-auto">
+    <main className="mx-auto flex flex-col md:w-full lg:h-auto">
       <div className="flex h-full overflow-y-auto">
         <Outlet />
       </div>
