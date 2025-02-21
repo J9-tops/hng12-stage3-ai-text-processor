3ed9bf47e4ad07a86ec310c6120ab07b5e11fe15
J9-tops <j9.tops@gmail.com>
2025-02-21 14:38:28 +0100

    style: added responsive layouts

--- src/components/ChatArea.tsx
+++ src/components/ChatArea.tsx
@@ -8,7 +8,7 @@ const ChatArea = () => {
 
   return (
     <section
-      className="mdlg:w-full xxs:pt-[130px] h-[calc(100vh-60px-99px)] w-screen overflow-x-hidden overflow-y-auto px-5 py-4 lg:pt-0"
+      className="mdlg:w-full h-[calc(100vh-60px-99px)] w-screen overflow-x-hidden overflow-y-auto px-5 py-4 lg:pt-0"
       aria-labelledby="chat area"
       tabIndex={0}
       role="region"
