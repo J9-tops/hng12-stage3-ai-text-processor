97006a2476a1818bb40c3e235d121a8b873872da
J9-tops <j9.tops@gmail.com>
2025-02-21 11:34:16 +0100

    style: added responsive layouts

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -3,13 +3,15 @@ import Header from "./Header";
 
 const RootLayout = () => {
   return (
-    <div className="root-layout mx-auto flex w-screen max-w-[80rem] flex-col overflow-hidden border-r border-l border-solid border-slate-500 bg-white shadow-2xl">
+    <div className="max-w-[80rem] lg:mx-auto lg:border-r lg:border-l lg:border-solid lg:border-slate-500">
       <Header />
       <div className="flex lg:block">
         <Outlet />
       </div>
     </div>
   );
+
+  // mx-auto hidden w-screen max-w-[80rem] flex-col overflow-hidden border-r border-l border-solid border-slate-500 bg-white shadow-2xl
 };
 
 export default RootLayout;
